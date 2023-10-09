5e65b646610d376378fcf1300ecbf2ded2846717
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-09 13:57:40 +0200

    setup

---
+++ spacewar.py
@@ -0,0 +1,16 @@
+#Spacewar by @coerres
+import os
+import random
+
+#Import the Turtle
+import turtle
+turtle.fd(0)
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1)
+turtle.tracer(1)
+
+
+
+delay = input("Press enter to exit. > ")
