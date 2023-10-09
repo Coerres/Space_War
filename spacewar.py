180b3d942d074f62a46caa2bab18d4374137853f
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-09 14:11:41 +0200

    added class 'sprite'

--- spacewar.py
+++ spacewar.py
@@ -8,8 +8,28 @@ turtle.fd(0)
 turtle.speed(0)
 turtle.bgcolor("black")
 turtle.ht()
+#This saves memory
 turtle.setundobuffer(1)
+#This speeds up drawing
 turtle.tracer(1)
+
+class Sprite(turtle.Turtle):
+        def __init__(self, spriteshape, color, startx, starty):
+                turtle.Turtle.__init__(self, shape = spriteshape)
+                self.speed(0)
+                self.penup
+                self.color(color)
+                self.fd(0)
+                self.goto(startx, starty)
+
+                self.speed = 1
+
+#Create my sprites
+player = Sprite("triangle", "white", 0, 0)
+
+
+
+
 
 
 
